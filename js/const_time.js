e30e4ce7b1d14a3335cabfc111e792f738ff6aca
ksigei <sigeikiprono4@gmail.com>
2023-01-28 22:05:10 +0300

    Constant time

--- js/const_time.js
+++ js/const_time.js
@@ -1,8 +1,8 @@
 // O(1) - Constant time:
 
 function log(array) {
-  console.log(array[0]);
-  console.log(array[1]);
+  console.log(array[6]);
+//   console.log(array[1]);
 }
 
 log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
